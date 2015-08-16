6d393118b9a55c07871ed50684bfac799407ac29
Dave Briccetti <daveb@davebsoft.com>
2015-08-16 16:42:10 -0700

    Add inventory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, session
+from invitem import InvItem
 from place import Place
 
 STARTING_PLACE_INDEX = 0
@@ -7,6 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     session['placeIndex'] = STARTING_PLACE_INDEX
+    session['inventory'] = []
     return redirect(places[STARTING_PLACE_INDEX].path)
 
 @app.route("/<newPath>")
@@ -17,9 +19,26 @@ def show_place(newPath):
     if newDest in transitions[place]:
         place = newDest
         session['placeIndex'] = places.index(place)
-    return render_template("advent.html", place=place, destinations=transitions[place])
+    carryingItems = [itemsByShortName[shortName] for shortName in session['inventory']]
+    availableItems = [item for item in place.items if item.shortName not in session['inventory']]
+    availableItems.sort(key=lambda i: i.title)
+    return render_template("advent.html", place=place, destinations=transitions[place],
+        carryingItems=carryingItems, availableItems=availableItems)
 
-pumpkin = Place('pumpkin', 'Arduino-Powered Pumpkin', audio='135498__compusician__halloween-003-wav-120b.wav')
+@app.route("/getItem/<item>")
+def getItem(item):
+    place = places[session['placeIndex']]
+    inv = session['inventory']
+    if item not in inv:
+        inv.append(item)
+    return redirect(place.path)
+
+paint = InvItem('paint', 'White spray paint')
+invItems = (paint,)
+itemsByShortName = {i.shortName: i for i in invItems}
+
+pumpkin = Place('pumpkin', 'Arduino-Powered Pumpkin',
+    audio='135498__compusician__halloween-003-wav-120b.wav', items=(paint,))
 monster = Place('monster', 'Flying Spaghetti Monster')
 camera  = Place('camera',  'Government Spy Camera')
 trail   = Place('trail',   'Mountain Bike Trail')
@@ -35,4 +54,4 @@ transitions = {
 }
 
 app.secret_key = 'the dog flies at noon'
-app.run()
+app.run(debug=True)
--- place.py
+++ place.py
@@ -1,5 +1,6 @@
 class Place(object):
-    def __init__(self, path, title, audio=None):
+    def __init__(self, path, title, audio=None, items=()):
         self.path = path
         self.title = title
         self.audio = audio
+        self.items = items
